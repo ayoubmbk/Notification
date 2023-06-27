967541ea240ee9736606530e9725cbc14c0695d2
ayoumbk <ayoub.mabrouk@esprit.tn>
2023-06-27 12:03:47 +0100

    first

---
+++ src/test/java/com/example/MicroServiceRealisation/MicroServiceRealisationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MicroServiceRealisation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceRealisationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
